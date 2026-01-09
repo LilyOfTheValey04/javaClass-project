24cee033cd2175f8f38098787a9ba5dc5516d7c1
Lily <lilymaria044@gmail.com>
2026-01-09 15:38:56 +0200

    add controllers

---
+++ src/main/java/com/controller/PaymentController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/payments")
+@Validated
+public class PaymentController {
+}
---
+++ src/main/java/com/controller/ReviewController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reviews")
+@Validated
+public class ReviewController {
+}
