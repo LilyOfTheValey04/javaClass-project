036194d801b20a5089df5c83faebaa54ded5fb19
Lily <lilymaria044@gmail.com>
2026-01-09 14:27:50 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/startup/JavaProjectTuApplication.java
@@ -0,0 +1,13 @@
+package com.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectTuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectTuApplication.class, args);
+	}
+
+}
