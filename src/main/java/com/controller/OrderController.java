5ee91243d72b1f9ee60cf5b3322dc7731aa5c7cb
Lily <lilymaria044@gmail.com>
2026-01-15 10:28:58 +0200

    add order controller logic

--- src/main/java/com/controller/OrderController.java
+++ src/main/java/com/controller/OrderController.java
@@ -1,13 +1,57 @@
 package com.controller;
 
+import com.dto.orderDTO.OrderRequestDTO;
+import com.dto.orderDTO.OrderResponseDTO;
+import com.mapper.OrderMapper;
+import com.model.Order;
+import com.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/orders")
 @Validated
 public class OrderController {
+
+    private final OrderService orderService;
+    private final OrderMapper orderMapper;
+
+    @PostMapping()
+    public ResponseEntity<OrderResponseDTO> createOrder(@Valid @RequestBody OrderRequestDTO requestDTO) {
+        Order order = orderService.createOrder(requestDTO);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(orderMapper.toOrderResponseDTO(order));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> getOrderById(@PathVariable Long id) {
+        Order order = orderService.getOrderById(id);
+        return new ResponseEntity<>(orderMapper.toOrderResponseDTO(order), HttpStatus.OK);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponseDTO>> getAllOrders() {
+        List<OrderResponseDTO> allOrders = orderService.getAllOrders()
+                .stream()
+                .map(orderMapper::toOrderResponseDTO)
+                .toList();
+
+        return ResponseEntity.ok(allOrders);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+
+        orderService.deleteOrderById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
