e9250a6adfb88695134041acf6e9858a1b935f28
Lily <lilymaria044@gmail.com>
2026-01-15 12:53:52 +0200

    add user mapper logic

--- src/main/java/com/mapper/UserMapper.java
+++ src/main/java/com/mapper/UserMapper.java
@@ -1,8 +1,20 @@
 package com.mapper;
 
+import com.dto.userDTO.UserResponseDTO;
+import com.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+
+import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
+    @Mapping(target = "materials", source = "materials")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "admin", constant = "false")
+
+    UserResponseDTO toUserResponseDTO(User user);
+
+    List<UserResponseDTO> toUserResponseDTOList(List<User> users);
 }
