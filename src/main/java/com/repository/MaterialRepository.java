3103b076287830b5e73ad1260d646e2002869df8
Lily <lilymaria044@gmail.com>
2026-01-15 10:25:48 +0200

    add method

--- src/main/java/com/repository/MaterialRepository.java
+++ src/main/java/com/repository/MaterialRepository.java
@@ -1,9 +1,14 @@
 package com.repository;
 
 import com.model.Material;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MaterialRepository extends JpaRepository<Material, Long> {
+    Optional<Material> findActiveById(@Param("id") Long id);
 }
--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.repository;
 
 import com.model.User;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByIdAndDeletedFalse(Long id);
 }
