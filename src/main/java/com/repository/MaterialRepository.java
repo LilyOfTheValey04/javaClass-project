19cceb0a16c3e375ccf4aae92654c203aaad581a
Lily <lilymaria044@gmail.com>
2026-01-15 10:27:56 +0200

    format

--- src/main/java/com/repository/MaterialRepository.java
+++ src/main/java/com/repository/MaterialRepository.java
@@ -1,7 +1,6 @@
 package com.repository;
 
 import com.model.Material;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.repository;
 
 import com.model.User;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
