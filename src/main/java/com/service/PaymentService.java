0026409879f7f3e3e8568b0d51163c067ce4c51f
Lily <lilymaria044@gmail.com>
2026-01-15 10:28:26 +0200

    add services

--- src/main/java/com/service/PaymentService.java
+++ src/main/java/com/service/PaymentService.java
@@ -1,9 +1,62 @@
 package com.service;
 
+import com.dto.paymentDTO.PaymentRequestDTO;
+import com.exception.ResourceNotFound;
+import com.model.Order;
+import com.model.Payment;
+import com.repository.OrderRepository;
+import com.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PaymentService {
+    private final OrderRepository orderRepository;
+    private final PaymentRepository paymentRepository;
+
+    @Transactional
+    public Payment createPayment(PaymentRequestDTO requestDTO) {
+
+        Order order = orderRepository.findById(requestDTO.orderId())
+                .orElseThrow(() -> new ResourceNotFound(Order.class, requestDTO.orderId()));
+
+        Payment payment = Payment.builder()
+                .order(order)
+                .build();
+
+        paymentRepository.save(payment);
+        return payment;
+
+    }
+
+    public List<Payment> getAllPayments() {
+        List<Payment> payments = paymentRepository.findAll();
+        return payments;
+    }
+
+    public Payment getPaymentById(Long id) {
+
+        Payment payment = paymentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFound(Payment.class, id));
+
+        return payment;
+    }
+
+    @Transactional
+    public void deletePaymentById(Long id) {
+        Payment payment = paymentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFound(Payment.class, id));
+
+        if (payment.getOrder() != null) {
+            payment.getOrder().setPayment(null);
+        }
+
+        paymentRepository.flush();
+        paymentRepository.deleteById(id);
+    }
 }
