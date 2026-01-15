faa302c0d57b833e05cc16781ea41f7f7ecaf6e0
Lily <lilymaria044@gmail.com>
2026-01-15 10:31:17 +0200

    add payment mapper logic

--- src/main/java/com/mapper/PaymentMapper.java
+++ src/main/java/com/mapper/PaymentMapper.java
@@ -1,8 +1,24 @@
 package com.mapper;
 
+import com.dto.paymentDTO.PaymentResponseDTO;
+import com.model.Payment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+
+import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PaymentMapper {
+
+    @Mapping(target = "orderId", source = "order.id")
+    @Mapping(target = "uuid", source = "order.uuid")
+    @Mapping(target = "buyerName", source = "order.buyer.name")
+    @Mapping(
+            target = "totalPrice",
+            expression = "java(payment.getOrder().getPrice().add(payment.getOrder().getDeliveryPrice()))"
+    )
+    PaymentResponseDTO toPaymentResponseDTO(Payment payment);
+
+    List<PaymentResponseDTO> toPaymentResponseDTOList(List<Payment> payments);
 }
