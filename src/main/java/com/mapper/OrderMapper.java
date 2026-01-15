08397b9c5e03b0448ce2982ce6953ae1736e0017
Lily <lilymaria044@gmail.com>
2026-01-15 10:29:18 +0200

    add order mapper logic

--- src/main/java/com/mapper/OrderMapper.java
+++ src/main/java/com/mapper/OrderMapper.java
@@ -1,8 +1,25 @@
 package com.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
+import com.dto.orderDTO.OrderResponseDTO;
+import com.model.Order;
+import org.mapstruct.*;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface OrderMapper {
+
+    @Mapping(target = "buyerName", source = "buyer.name")
+    @Mapping(target = "materialTitle", source = "material.name")
+    @Mapping(target = "uniqueNumber", source = "uuid")
+    @Mapping(target = "price", source = "price")
+    @Mapping(target = "deliveryPrice", source = "deliveryPrice")
+    @Mapping(target = "quantity", source = "quantity")
+    @Mapping(target = "dateCreated", source = "dateCreated")
+    @Mapping(
+            target = "totalPrice",
+            expression = "java(order.getPrice().add(order.getDeliveryPrice()))"
+    )
+    OrderResponseDTO toOrderResponseDTO(Order order);
 }
+
+
+
