f2b66a962b8b9f8e2433fe62bd7c68312d2565e7
Lily <lilymaria044@gmail.com>
2026-01-15 10:33:21 +0200

    add userDTOs

---
+++ src/main/java/com/dto/UserDTO/UserRequestDTO.java
@@ -0,0 +1,13 @@
+package com.dto.UserDTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRequestDTO(@NotBlank String username,
+                             @NotBlank String password,
+                             String name,
+                             @Email String email,
+                             String phoneNumber) {
+
+
+}
---
+++ src/main/java/com/dto/UserDTO/UserResponseDTO.java
@@ -0,0 +1,16 @@
+package com.dto.UserDTO;
+
+import com.dto.material.MaterialCreateRequestDTO;
+
+import java.util.List;
+
+public record UserResponseDTO(Long id,
+                              boolean admin,
+                              String username,
+                              String name,
+                              String email,
+                              String phoneNumber
+                             // List<MaterialCreateRequestDTO> purchases, //purchased material //fix dto
+                            //  List<MaterialCreateRequestDTO> materials //all created materials //fix dto
+                             ){
+}
